32cfe21c857c907b59df3141a6a169d7cb034565
pdidit <L0trS0mL0th>
2021-02-28 23:12:29 +0000

    init commit

---
+++ mcpdiscovery/src/main/java/com/microservice/mcpdiscovery/McpdiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.mcpdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpdiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpdiscoveryApplication.class, args);
+    }
+
+}
---
+++ mcpdiscovery/src/test/java/com/microservice/mcpdiscovery/McpdiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.mcpdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpdiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
